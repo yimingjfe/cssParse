7d3d49cd7165a2d17ba9e3c7d02b277114260cf1
Ming <527990618@163.com>
2018-04-22 23:51:49 +0800

    学习sourcemap

---
+++ rpn/bin/rpn.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var fs = require("fs");
+var rpn = require("rpn");
+ 
+process.argv.slice(2).forEach(function (file) {
+  var input = fs.readFileSync(file);
+  var output = rpn.compile(input, {
+    originalFilename: file
+  }).toStringWithSourceMap({
+    file: file.replace(/.[w]+$/, ".js.map")
+  });
+  var sourceMapFile = file.replace(/.[w]+$/, ".js.map");
+  fs.writeFileSync(file.replace(/.[w]+$/, ".js"),
+                   output.code + "n//# sourceMappingURL=" + sourceMapFile);
+  fs.writeFileSync(sourceMapFile, output.map);
+});
